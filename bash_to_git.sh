3afcf9e1d77dabff2ecbb6eb2fe2ea4ff5c383e5
karpovdenis <karpovdenis@iMac-Denis.local>
2022-01-04 11:51:13 +0300

    $commit_text

---
+++ bash_to_git.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+flake8
+if [ $? -eq 0 ]
+then
+  echo "flake8:   - ОК"
+else
+  echo "Не пройдено тестирование:   - flake8"
+  exit 1
+fi
+python manage.py test
+if [ $? -eq 0 ]
+then
+  echo "python manage.py test:   - ОК"
+else
+  echo "Не пройдено тестирование:   - python manage.py test"
+  exit 1
+fi
+git status
+git add .
+read -p 'Введите комментарий для текущего коммита: ' commit_text
+git commit -m "\$commit_text"
+git push git push git@github.com:KarpovDenis74/banking_analytics.git
