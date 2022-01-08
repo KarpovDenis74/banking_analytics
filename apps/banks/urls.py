7d859dc21c654469f35a11f479bb2eee9e8ad70f
karpovdenis <karpovdenis@iMac-Denis.local>
2022-01-08 22:43:45 +0300

    Fix urls in nav.html

--- apps/banks/urls.py
+++ apps/banks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from apps.banks.views import BankView
+from apps.banks.tasks import (get_regions, get_accounts, get_bics)
 
 app_name = 'banks'
 
@@ -10,5 +11,8 @@ urlpatterns = [
     path('view_regions/', BankView.view_regions, name='view_regions'),
     path('view_accounts/', BankView.view_accounts, name='view_accounts'),
     # Запросы к API ЦБ РФ
+    path('get_regions/', get_regions, name='get_regions'),
+    path('get_accounts/', get_accounts, name='get_accounts'),
+    path('get_bics/', get_bics, name='get_bics'),
 
 ]
